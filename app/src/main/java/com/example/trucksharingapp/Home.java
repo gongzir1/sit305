ad784c2ac01d27c1a070ac93e2d843b25c1231d8
zirui gong <gongzir@deakin.edu.au>
2022-05-26 15:12:09 +1000

    finish call book

---
+++ app/src/main/java/com/example/trucksharingapp/Home.java
@@ -0,0 +1,90 @@
+package com.example.trucksharingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends AppCompatActivity implements OrderAdapter.OnRowClickListener {
+    RecyclerView rv_Order;
+    OrderAdapter orderAdapter;
+    List<Order> orderList=new ArrayList<>();
+    ImageButton dropDownMenu;
+    Button add_order;
+    private static Integer[]newImageList={R.drawable.image,R.drawable.image,R.drawable.image,R.drawable.image,R.drawable.image,R.drawable.image};
+    private static String[]newNameList={"order1","order2","order3","order4","order5","order6"};
+    private static String[]newDetailList={"this is order1","this is order2","this is order3","this is order4","this is order5","this is order6"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        rv_Order=findViewById(R.id.rv_order);
+        orderAdapter=new OrderAdapter(orderList,Home.this,this);
+        RecyclerView.LayoutManager vertical_manager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        rv_Order.setAdapter(orderAdapter);
+        rv_Order.setLayoutManager(vertical_manager);
+        for(int i=0;i<newImageList.length;i++){
+            Order order=new Order(newImageList[i],newNameList[i],newDetailList[i], R.drawable.changetask);
+            orderList.add(order);
+        }
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("user");
+        dropDownMenu=findViewById(R.id.imageButton);
+        dropDownMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(Home.this, dropDownMenu);
+                popup.getMenuInflater()
+                        .inflate(R.menu.menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getTitle().equals("Home")){
+                            Toast.makeText(getApplicationContext(), "home",Toast.LENGTH_SHORT).show();
+                        }
+                        else if (item.getTitle().equals("My orders")){
+                            Intent intentorder= new Intent(getApplicationContext(),My_orders_new.class);
+                            intentorder.putExtra("user", username);
+                            startActivity(intentorder);
+                            Toast.makeText(getApplicationContext(), "my orders",Toast.LENGTH_SHORT).show();
+                        }
+                        else if(item.getTitle().equals("Account")){
+                           // Toast.makeText(getApplicationContext(), "account",Toast.LENGTH_SHORT).show();
+                        Intent intentaccount=new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intentaccount);
+                        }
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+        add_order=findViewById(R.id.btn_add_order);
+        add_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentadd=new Intent(getApplicationContext(),Add_order.class);
+                startActivity(intentadd);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(int id) {
+        Intent intentdetail=new Intent(getApplicationContext(),Order_details.class);
+        startActivity(intentdetail);
+    }
+}
---
+++ app/src/main/java/com/example/trucksharingapp/MapsActivity.java
@@ -0,0 +1,110 @@
+package com.example.trucksharingapp;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.trucksharingapp.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    Button call,book;
+    static int PERMISSION_CODE= 100;
+    TextView pickup,dropoff,time,money;
+    Integer distance=100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+        Intent intent=getIntent();
+        String from=intent.getStringExtra("from");
+        String to=intent.getStringExtra("to");
+        call=findViewById(R.id.calldriver);
+        book=findViewById(R.id.book);
+        pickup=findViewById(R.id.pickup);
+        dropoff=findViewById(R.id.dropOff);
+        time=findViewById(R.id.time);
+        money=findViewById(R.id.fare);
+        pickup.setText("Pick up at: "+from);
+        dropoff.setText("Drop off at: "+to);
+
+        time.setText("approxi time is: "+distance/10);
+        money.setText("approxi fare is: "+distance*1.5);
+
+
+
+
+        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(MapsActivity.this,new String[]{Manifest.permission.CALL_PHONE},PERMISSION_CODE);
+
+        }
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String phoneno = "123321";
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:"+phoneno));
+                startActivity(i);
+
+            }
+        });
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MapsActivity.this,"successfully book!",Toast.LENGTH_SHORT).show();
+                Intent intent1=new Intent(getApplicationContext(),CheckoutActivity.class);
+                startActivity(intent1);
+
+            }
+        });
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+}
